de1dfec0a46b24e7315f7dcf889d51ae0612619f
Hilda <hildagichohi27@gmail.com>
2026-05-08 12:56:57 +0300

    Updated HTML,CSS and JavaScript files

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
     
     actionBtn.addEventListener("click", () => {
         // Hilda, try changing this alert message!
-        alert("Hello Hilda! You successfully clicked the button.");
+        alert("Button clicked successfully.");
     });
 });
