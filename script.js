c326ee0d3b413a43464e1375d33e64213c246a9e
David Mbugua <47davidmbugua23@gmail.com>
2026-05-08 10:37:02 +0300

    Initial commit for Hilda onboarding project

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const actionBtn = document.getElementById("action-btn");
+    
+    actionBtn.addEventListener("click", () => {
+        // Hilda, try changing this alert message!
+        alert("Hello Hilda! You successfully clicked the button.");
+    });
+});
